538c5f55d2f1cd0785819042cb14699a68f361f6
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2018-11-27 14:13:56 -0600

    Making Java WebSocket Client report it's ID on the messages.
    Signed-off-by: Joakim Erdfelt <joakim.erdfelt@gmail.com>

--- src/main/java/com/example/ws/ClientStarter.java
+++ src/main/java/com/example/ws/ClientStarter.java
@@ -11,24 +11,34 @@ import javax.websocket.WebSocketContainer;
 import org.eclipse.jetty.util.component.LifeCycle;
 import com.example.services.BroadcastClientEndpoint;
 
-public class ClientStarter {
-    public static void main(final String[] args) throws Exception {
-        final String client = UUID.randomUUID().toString().substring(0, 8);
+public class ClientStarter
+{
+    public static void main(final String[] args) throws Exception
+    {
+        String clientId = UUID.randomUUID().toString().substring(0, 8);
+
+        if (args.length > 0)
+        {
+            clientId = args[0];
+        }
 
         final WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         final String uri = "ws://localhost:8080/broadcast";
 
-        try (Session session = container.connectToServer(BroadcastClientEndpoint.class, URI.create(uri))) {
-            for (int i = 1; i <= 1000; ++i) {
-                TimeUnit.SECONDS.sleep(10);
-                session.getBasicRemote().sendText("Message from JavaClient #" + i);
+        try (Session session = container.connectToServer(BroadcastClientEndpoint.class, URI.create(uri)))
+        {
+            for (int i = 1; i <= 1000; ++i)
+            {
+                TimeUnit.SECONDS.sleep(5);
+                session.getBasicRemote().sendText(String.format("Message %,d from JavaClient [%s]", i, clientId));
             }
         }
 
         // JSR-356 has no concept of Container lifecycle.
         // (This is an oversight on the spec's part)
         // This stops the lifecycle of the Client WebSocketContainer
-        if (container instanceof LifeCycle) {
+        if (container instanceof LifeCycle)
+        {
             ((LifeCycle) container).stop();
         }
     }
